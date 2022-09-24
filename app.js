bca936fdf32b7eb12b6095e48a24aa2158e58e16
MuratÇamurlu <mcamurlu1923@gmailcom>
2022-09-24 16:56:35 +0300

    sayfa tamam

---
+++ app.js
@@ -0,0 +1,79 @@
+const apiKey = "ea9f8036314eb976c178ee911f31c27b";
+
+const form = document.querySelector("form");
+const formInput = document.querySelector("input");
+const warning = document.querySelector(".warning");
+const resultArea = document.querySelector(".cities");
+
+const currentCity = document.querySelector(".aktuel");
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  getWeatherData();
+};
+const getWeatherData = async () => {
+  let input = formInput.value;
+  const cityCart = document.createElement("div");
+  cityCart.classList.add("city");
+  let weatherType = "metric";
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=${weatherType}`;
+
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    const { main, name, sys, weather } = data;
+    const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@4x.png`;
+
+    cityCart.innerHTML = `<div class="head">
+                          <h2 class="cityName"><span>${name}</span><sup>${
+      sys.country
+    }</sup>
+                          </h2>
+                          <div class="closeIcon">X</div>
+                      </div>
+  
+                      <div class="cityTemp">${Math.round(
+                        main.temp
+                      )}<sup>°C</sup></div>
+  
+                      <img class="cityIcon" src="${iconUrl}">
+                      <p>${weather[0].description}</p>`;
+
+    resultArea.appendChild(cityCart);
+    warning.innerText = "";
+    form.reset();
+  } catch (error) {
+    warning.innerText = `The name you are trying to enter is incorrect`;
+    form.reset();
+  }
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  const { main, name, sys, weather } = data;
+  const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@4x.png`;
+
+  cityCart.innerHTML = `<div class="head">
+                        <h2 class="cityName"><span>${name}</span><sup>${
+    sys.country
+  }</sup>
+                        </h2>
+                        <div class="closeIcon">X</div>
+                    </div>
+
+                    <div class="cityTemp">${Math.round(
+                      main.temp
+                    )}<sup>°C</sup></div>
+
+                    <img class="cityIcon" src="${iconUrl}">
+                    <p>${weather[0].description}</p>`;
+
+  resultArea.appendChild(cityCart);
+};
+resultArea.onclick = (e) => {
+  if (e.target.className === "closeIcon") {
+    e.target.closest(".city").remove();
+  }
+};
